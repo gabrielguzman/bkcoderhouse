9999949c67d4d7f2198ddaaed6138851643a7ed3
gabrielguzman <gabrielguzman147.gg@gmail.com>
2023-05-11 15:39:54 -0300

    Listado de productos con express-handlebars

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import ProductManager from "../productManager.js";
 
 const viewsRouter = Router();
+const productManager = new ProductManager('./productos.json');
 
-viewsRouter.get('/', (req,res)=>{
-    res.render('index');
+
+viewsRouter.get('/', async (req,res)=>{
+    const products = await productManager.getProducts();
+    res.render('home', {products});
 })
 
 export {viewsRouter};
