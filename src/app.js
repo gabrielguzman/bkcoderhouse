47c4507d1e8500b1eaf021debe7d513768c8c63d
gabrielguzman <gabrielguzman147.gg@gmail.com>
2023-05-11 14:52:12 -0300

    configuracion express-handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from "express";
+import handlerbars from 'express-handlebars';
 import { productsRouter } from "./routes/productsRouter.js";
 import { cartsRouter } from "./routes/cartsRouter.js";
+import { viewsRouter } from "./routes/viewsRouter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//set handlebars
+app.engine('handlebars', handlerbars.engine());
+app.set('views', 'views/');
+app.set('view engine', 'handlebars');
+//set public
+app.use(express.static('public'));
+
+app.use('/', viewsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter); 
 
